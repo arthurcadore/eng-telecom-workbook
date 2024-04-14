e7ed4e42c1ed6abdb6f79f283fccbc23e5ac827e
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-04-14 03:55:59 +0000

    :sparkles: added quantizatin to PCM

--- COM1/modulation-PCM/PCM1.m
+++ COM1/modulation-PCM/PCM1.m
@@ -17,7 +17,7 @@ T = 1/f_signal;
 
 % Defining the sinal period.
 t_inicial = 0;
-t_final = 0.5;
+t_final = 0.14;
 
 % "t" vector, correspondig to the time period of analysis, on time domain.
 t = [t_inicial:Ts:t_final];
@@ -30,15 +30,46 @@ impulse_train(mod(t, 1/fs) == 0) = 1;
 
 signal_sampled = signal .* impulse_train;
 
+
+% Quantidade de níveis desejada (tirando o 0)
+n=3
+num_levels = 2^n;
+
+% Gerando os níveis de quantização automaticamente
+levels = linspace(-1, 1, num_levels+1);
+
+% Quantização
+quantized_signal = zeros(size(signal_sampled));
+
+for i = 1:length(signal_sampled)
+    if signal_sampled(i) <= levels(1)
+        quantized_signal(i) = levels(1);
+    elseif signal_sampled(i) >= levels(end)
+        quantized_signal(i) = levels(end);
+    else
+        for j = 1:length(levels)-1
+            if signal_sampled(i) >= levels(j) && signal_sampled(i) < levels(j+1)
+                if abs(signal_sampled(i) - levels(j)) < abs(signal_sampled(i) - levels(j+1))
+                    quantized_signal(i) = levels(j);
+                else
+                    quantized_signal(i) = levels(j+1);
+                end
+                break;
+            end
+        end
+    end
+end
+
 figure(1)
-subplot(311)
+subplot(411)
 plot(t,signal)
 xlim([0 5*T])
-subplot(312)
+subplot(412)
 stem(t,impulse_train, 'MarkerFaceColor', 'b')
 xlim([0 5*T])
-subplot(313)
+subplot(413)
 stem(t,signal_sampled, 'LineStyle','none', 'MarkerFaceColor', 'b')
 xlim([0 5*T])
-
-
+subplot(414)
+stem(t,quantized_signal, 'LineStyle','none', 'MarkerFaceColor', 'b')
+xlim([0 5*T])
