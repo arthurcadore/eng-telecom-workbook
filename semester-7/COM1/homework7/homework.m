7bbc8ca8b2f75ebd23688cd90390cd518a5ebc98
arthurcadore <arthurbarcella.ifsc@gmail.com>
2024-07-09 13:44:28 +0000

    :sparkles: updated code

--- semester-7/COM1/homework7/homework.m
+++ semester-7/COM1/homework7/homework.m
@@ -1,11 +1,13 @@
 clc; close all; clear all;
 
 M = 16;
-info = randi([0 M-1],1,100);
+Vector_lenght = 1000;
+info = randi([0 M-1],1,Vector_lenght);
 info_mod = qammod(info, M);
 SNR = 20;
 
-info_r = awgn(info_mod, SNR, 'measured');
+% info_r = awgn(info_mod, SNR, 'measured');
+info_r = info_mod;x
 info_demod = qamdemod(info_r, M);
 [num_erro(SNR+1), SER(SNR+1)] = symerr(info, info_demod);
 
@@ -19,17 +21,26 @@ info_r_real = real(info_r);
 
 info_r_imag = imag(info_r);
 
-% create a 4-PAM modulation for the real part: 
+% ========================================
 
-info_r_real_mod = pammod(info_r_real, 4);
+% Defining the ratebit: 
 
-% create a 4-PAM modulation for the imaginary part:
+rb = 1; 
+rs = rb/log2(M);
+ts = 1/rs;
 
-info_r_imag_mod = pammod(info_r_imag, 4);
+% ========================================
 
-% Plot the real part of the signal in the exis of time: 
+% Defining the time vector:
 
-figure(3)
-subplot(2,1,1)
-plot(info_r_real_mod)
+t = [0:ts:Vector_lenght*ts-ts];
+
+% upsample and filter the real part
+
+info_r_real_up = upsample(info_r_real, rs);
+
+filtro = ones(1, rs);
+
+info_r_real_tx = filter(filtro, 1, info_r_real_up);
+
 
