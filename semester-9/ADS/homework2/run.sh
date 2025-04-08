fe2805af9855ac43b253edf93fbbea88c35a8782
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-04-08 11:04:33 -0300

    :sparkles: added new code to main

--- semester-9/ADS/homework2/run.sh
+++ semester-9/ADS/homework2/run.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-SCENARIO_ID=i1666
 TOPOLOGY_FILE=scenario.imn
 BANDWIDTH=100000000
+# SCENARIO_ID=i2002
 # DELAY=10000
 # FLUXES=1
 
 # Create a log directory if it doesn't exist
 mkdir -p ./log
 
-echo "SCENARIO 3"
 echo "========================================================="
 echo "Starting IMUNES simulation with scenario ID: $SCENARIO_ID"
-echo "Using topology file: $TOPOLOGY_FILE"
 
 # Run IMUNES in background and redirect all output to log
-sudo imunes -b -e $SCENARIO_ID $TOPOLOGY_FILE
+sudo imunes -b -e $SCENARIO_ID $TOPOLOGY_FILE > /dev/null
+sleep 2
 
 echo "========================================================="
 echo "Simulation started, applying commands..."
 
 # Link configurations
-sudo vlink -bw $BANDWIDTH -dly $DELAY router1:pc1@$SCENARIO_ID
-sudo vlink -bw $BANDWIDTH -dly $DELAY router1:pc3@$SCENARIO_ID
-sudo vlink -bw $BANDWIDTH -dly $DELAY router2:pc2@$SCENARIO_ID
-sudo vlink -bw $BANDWIDTH -dly $DELAY router2:pc4@$SCENARIO_ID
-sudo vlink -bw $BANDWIDTH -dly $DELAY router2:router1@$SCENARIO_ID
+sudo vlink -bw $BANDWIDTH -dly $DELAY router1:pc1@$SCENARIO_ID > /dev/null
+sudo vlink -bw $BANDWIDTH -dly $DELAY router1:pc3@$SCENARIO_ID > /dev/null
+sudo vlink -bw $BANDWIDTH -dly $DELAY router2:pc2@$SCENARIO_ID > /dev/null
+sudo vlink -bw $BANDWIDTH -dly $DELAY router2:pc4@$SCENARIO_ID > /dev/null
+sudo vlink -bw $BANDWIDTH -dly $DELAY router2:router1@$SCENARIO_ID > /dev/null
+sleep 2
 
 # Check status (optional)
-sudo vlink -s router1:pc1@$SCENARIO_ID 
-sudo vlink -s router1:pc3@$SCENARIO_ID 
-sudo vlink -s router2:pc2@$SCENARIO_ID 
-sudo vlink -s router2:pc4@$SCENARIO_ID 
-sudo vlink -s router2:router1@$SCENARIO_ID 
+sudo vlink -s router1:pc1@$SCENARIO_ID
+sudo vlink -s router1:pc3@$SCENARIO_ID
+sudo vlink -s router2:pc2@$SCENARIO_ID
+sudo vlink -s router2:pc4@$SCENARIO_ID
+sudo vlink -s router2:router1@$SCENARIO_ID
+sleep 2
 
 # Start iperf servers
 sudo himage pc2@$SCENARIO_ID iperf -s &> /dev/null &
@@ -40,6 +41,8 @@ sudo himage pc4@$SCENARIO_ID iperf -s &> /dev/null &
 
 # Generate background UDP traffic (mute output)
 sudo himage pc1@$SCENARIO_ID iperf -c 10.0.3.20 -u -t 100000 -b 10M &> /dev/null &
+sleep 2
+
 
 # Run TCP test (this is the one you want to see)
 echo "========================================================="
@@ -49,7 +52,9 @@ sudo himage pc3@$SCENARIO_ID iperf -c 10.0.4.20 -n 100M -P $FLUXES -i 1
 # Stop simulation
 echo "========================================================="
 echo "Stopping IMUNES simulation with scenario ID: $SCENARIO_ID"
-sudo imunes -b -e $SCENARIO_ID 
+sudo imunes -b -e $SCENARIO_ID > /dev/null
+echo "========================================================="
+echo "Simulation stopped"
 
 
 
